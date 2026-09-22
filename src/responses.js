46ef1ad04ae5a493651b04ee6a47a9ad240939a0
vni5104 <vickyn5104@gmail.com>
2026-09-21 20:11:24 -0400

    finished assignment

--- src/responses.js
+++ src/responses.js
@@ -1,11 +1,12 @@
 const respond = (request, response, statusCode, id, message) => {
-    const dataType= request.headers['accept'] === 'text/xml' ? 'text/xml' : 'application/json';
+    //default dataType to 'application/json' if it's not 'text/xml'
+    const dataType = request.headers['accept'] === 'text/xml' ? 'text/xml' : 'application/json';
 
     let content;
     if (dataType === 'text/xml') {
-        content = `<response><id>${id}</id><message>${message}</message></response>`;
+        content = `<response><message>${message}</message><id>${id}</id></response>`;
     } else {
-        content = JSON.stringify({id:id, message: message});
+        content = JSON.stringify({message: message, id: id});
     }
 
     response.writeHead(statusCode, { 'Content-Type': dataType});
@@ -13,11 +14,25 @@ const respond = (request, response, statusCode, id, message) => {
     response.end();
 }
 
+//Handle success response (which only has message field)
+const respondSuccess = (request, response, statusCode, message) => {
+    const dataType = request.headers['accept'] === 'text/xml' ? 'text/xml' : 'application/json';
 
-const id200 = 'success';
+    let content;
+    if (dataType === 'text/xml') {
+        content = `<response><message>${message}</message></response>`;
+    } else {
+        content = JSON.stringify({message: message});
+    }
+
+    response.writeHead(statusCode, { 'Content-Type': dataType});
+    response.write(content);
+    response.end();
+}
+
 const message200 = 'This is a successful response.';
 
-const success = (request, response) => respond(request, response, 200, id200, message200);
+const success = (request, response) => respondSuccess(request, response, 200, message200);
 
 const badRequest = (request, response) => {
     const protocol = request.connection.encrypted ? 'https' : 'http';
@@ -30,7 +45,7 @@ const badRequest = (request, response) => {
     const message = 'Missing valid query parameter set to true.';
 
     if (valid === 'true') {
-        respond(request, response, 200, id200, message200);
+        respondSuccess(request, response, 200, message200);
     } else {
         respond(request, response, 400, id, message);
     }
@@ -43,10 +58,10 @@ const unauthorized = (request, response) => {
     const loggedIn = url.searchParams.get('loggedIn');
 
     const id = 'unauthorized';
-    const message = 'Missing loggedIn query parameter set to yes';
+    const message = 'Missing loggedIn query parameter set to yes.';
 
     if (loggedIn === 'yes') {
-        respond(request, response, 200, id200, message200);
+        respondSuccess(request, response, 200, message200);
     } else {
         respond(request, response, 401, id, message);
     }
@@ -54,14 +69,14 @@ const unauthorized = (request, response) => {
 
 const forbidden = (request, response) => {
     const id = 'forbidden';
-    const message = 'You do not have access to this content';
+    const message = 'You do not have access to this content.';
 
     respond(request, response, 403, id, message);
 };
 
 const internal = (request, response) => {
     const id = 'internalError';
-    const message = 'Internal Server Error. Something went wrong';
+    const message = 'Internal Server Error. Something went wrong.';
 
     respond(request, response, 500, id, message);
 };
--- src/server.js
+++ src/server.js
@@ -16,40 +16,6 @@ const urlStruct = {
     default: responseHandler.notFound
 };
 
-const parseBody = (request, response, handler) => {
-    const body = [];
-
-    request.on('error', (err) => {
-        console.dir(err);
-        response.statusCode = 400;
-        response.end();
-    });
-
-    request.on('data', (chunk) => {
-        body.push(chunk);
-    });
-
-    request.on('end', () => {
-        const bodyString = Buffer.concat(body).toString();
-        const type = request.headers['content-type'];
-
-        console.log(bodyString);
-
-        if(type === 'application/json') {
-            request.body = JSON.parse(bodyString);
-        } else if (type === 'text/xml') {
-            const parser = new DOMParser();
-            request.body = parser.parseFromString(bodyString);
-        } else {
-            response.writeHead(400, { 'Content-Type': 'application/json' });
-            response.write(JSON.stringify({ error: 'invalid data format' }));
-            return response.end();
-        }
-
-        handler(request, response);
-    });
-}
-
 const onRequest = (request, response) => {
     const protocol = request.connection.encrypted ? 'https' : 'http';
     const parsedURL = new URL(request.url, `${protocol}://${request.headers.host}`);
